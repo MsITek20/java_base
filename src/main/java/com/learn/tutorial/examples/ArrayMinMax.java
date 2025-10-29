2c67dbf40de4cfa1dc7b96872fa8fda52d1979b4
Kamil Sitek <kamil.sitek98@interia.pl>
2025-10-29 23:21:07 +0100

    create tasks #1

---
+++ src/main/java/com/learn/tutorial/examples/ArrayMinMax.java
@@ -0,0 +1,26 @@
+package com.learn.tutorial.examples;
+
+/*
+Zadanie 5: Minimum i maksimum w tablicy
+
+Napisz program, który:
+- wczytuje najpierw liczbę n (ile liczb będzie wprowadzonych),
+- następnie wczytuje n liczb całkowitych,
+- wypisuje najmniejszą i największą z podanych liczb.
+
+Przykład:
+Wejście: 5 i wartości: 3 9 -2 0 9
+Wyjście: min = -2, max = 9
+
+Wskazówki:
+- Waliduj n > 0.
+- Nie sortuj – aktualizuj min i max podczas wczytywania.
+*/
+public class ArrayMinMax {
+    public static void main(String[] args) {
+        // TODO: Tu zaimplementuj rozwiązanie.
+    }
+
+
+}
+
---
+++ src/main/java/com/learn/tutorial/examples/SumOfDigits.java
@@ -0,0 +1,30 @@
+package com.learn.tutorial.examples;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/*
+Zadanie 1: Suma cyfr liczby
+
+Napisz program, który:
+- wczytuje dodatnią liczbę całkowitą (np. przy użyciu klasy Scanner),
+- oblicza sumę jej cyfr,
+- wypisuje wynik na standardowe wyjście.
+
+Przykłady:
+- 123 -> 6
+- 9005 -> 14
+- 7 -> 7
+
+Wskazówki:
+- Możesz użyć operacji % 10 i / 10, albo zamienić liczbę na tekst i iterować po znakach.
+- Pamiętaj o przypadkach brzegowych (bardzo duże liczby, zera w środku).
+*/
+public class SumOfDigits {
+    public static void main(String[] args) {
+        // TODO: Tu zaimplementuj rozwiązanie.
+        // Przykład startu: Scanner sc = new Scanner(System.in); int n = sc.nextInt();
+    }
+}
+
